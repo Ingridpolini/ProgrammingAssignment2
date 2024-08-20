517a46fcaa0601b519da1f6fb1e93028dd55243d
Ingridpolini <86085333+Ingridpolini@users.noreply.github.com>
2024-08-20 16:13:04 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions do matrix inversion for large datasets
 
-## Write a short comment describing this function
+## This function creates a matrix, gets the value of the matrix and then set and get the value of solve.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+  
+  set <- function(y){
+        x <<- y
+        m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  matrix(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## this function calls the inversion of the matrix with the special matrix created with the function above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)){
+              message("getting cached data")
+              return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
